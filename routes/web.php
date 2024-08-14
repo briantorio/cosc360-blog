26e743da973dffc5acb0d9c53b910c42320a96dc
Brian <briantorio11@gmail.com>
2024-08-14 15:14:35 +1000

    setup Laravel UI and Set Up Authentication

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,7 @@
 use App\Http\Controllers\PostController;
 
 Route::resource('posts', PostController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
